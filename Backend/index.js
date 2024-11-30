d7d56f27e897c50b3d74dea7d025d13bf9aa4138
Saurabh <sourabhpatil310@gmail.com>
2024-11-30 12:30:43 +0530

    Production

--- Backend/index.js
+++ Backend/index.js
@@ -20,8 +20,8 @@ const URI = process.env.MongoDBURI;
 //MongoDb Database Connection
 try {
   mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   });
   console.log(" Connected to MongoDB...");
 } catch (error) {
