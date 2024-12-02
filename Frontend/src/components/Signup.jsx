35893ee566ed4053d2d62b6de7da537d63294315
Saurabh Patil <113912628+Saurabhpatil310@users.noreply.github.com>
2024-12-02 19:58:12 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -17,7 +17,7 @@ function Signup() {
     email: data.email,
     password: data.password,
    }
-  await axios.post('http://localhost:4001/user/signup', userInfo)
+  await axios.post('https://bookstoreapp-backend-gxmj.onrender.com/user/signup', userInfo)
    .then((res)=>{
     console.log(res.data)
     if( res.data){
